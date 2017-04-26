bd8fc72a231daee798d263d50d1cb97a0ff8b2c3
Muhammed Karabulut <detraditionalization@gmail.com>
2017-04-27 01:58:15 +0300

    Sun rotation and lighting added

--- Assets/_Scripts/SunRotation.cs
+++ Assets/_Scripts/SunRotation.cs
@@ -1,16 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using MBK;
+
+
 
 public class SunRotation : MonoBehaviour {
+    [SerializeField]
+    float RotationSpeed;
+    [SerializeField]
+    Gradient SunColor;
+    [SerializeField]
+    bool IsRotating;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Light lgt;
+
+    Transform tr;
+
+    private void Awake()
+    {
+        tr = transform;
+        lgt = GetComponent<Light>();
+    }
+
+    private void LateUpdate()
+    {
+        if (IsRotating)
+            tr.Rotate(Vector3.right, RotationSpeed * Time.smoothDeltaTime);
+        lgt.color = SunColor.Evaluate((tr.rotation.eulerAngles.x % 360f) / 360f);        
+    }
+
 }
