5a65d616524247f0f67984663bd0edc508cb39d0
Muhammed Bedri Karabulut <muho_121@hotmail.com>
2017-04-28 18:19:30 +0300

    Textures lights and skybox redone. sky asset added

---
+++ Assets/_ScriptableObjects/UIManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Managers/UIManager")]
+public class UIManager : ScriptableObject {
+
+
+    private bool _isday = true;
+    public bool IsDay { get { return _isday; } 
+        set
+        {
+            _isday = value;
+
+            if (_isday)
+                sun.SetActive(true);
+            else
+                sun.SetActive(false);
+        }
+    }
+
+    private GameObject sun;
+
+    private void OnEnable()
+    {
+        sun = GameObject.FindWithTag("Sun");
+    }
+
+    public void ToggleDayNight()
+    {
+        IsDay = !IsDay;
+    }
+
+}
