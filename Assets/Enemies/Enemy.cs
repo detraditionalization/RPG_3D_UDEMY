7d421eba2ea983d9c9eefd5b66f8d87dc89a7746
Muhammed Bedri Karabulut <muho_121@hotmail.com>
2017-05-16 13:10:33 +0300

    Terrain Toolkit added adn terrain changed

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -18,4 +18,6 @@ public class Enemy : MonoBehaviour {
     }
 
 
+
+
 }
---
+++ Assets/_Scripts/VRRenderScaler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+
+public class VRRenderScaler : MonoBehaviour {
+
+    [Range(0.8f,3f)]
+    [SerializeField] private float renderScale;
+
+
+    private void Start()
+    {
+        VRSettings.renderScale = renderScale;
+    }
+}
